9eb67e082ed31d735564656c876e138c060030e0
paul <sl.zhou@upltv.com>
2018-10-18 11:02:38 +0800

    修改样式

--- source/_static/js/custom.js
+++ source/_static/js/custom.js
@@ -1,4 +1,6 @@
 $("footer").html($("footer").html().replace(/Built with(.)*/, ' '))
 $(".ethical-sidebar").html(' ')
 
+
 $('.rst-current-version').addClass('shift-up').removeAttr('data-toggle')
+$('.rst-other-versions').css("display", "block")
